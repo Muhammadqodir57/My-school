ffe51b7f8a26b2bf8f0e75286101a8c436929149
Muhammadqodir_57 <mirzamatovanasiba@gmail.com>
2024-01-22 20:00:37 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// //-------------------CREATE-ELEMENT--------------//
+// const elTitle = document.createElement("h3")
+// const elList = document.createElement("ul")
+// const elItem = document.createElement("li")
+// const elLink = document.createElement("a")
+
+
+
+
+
+
+
+
+
+
+// //-------------------INNER-TEXT--------------//
+// elTitle.innerText = "MySchool"
+// elLink.innerText = "Home"
+
+
+// //-------------------APPEND--------------//
+
+
+// document.body.append(elTitle)
+// document.body.append(elList)
+// document.body.elList.append(elItem)
+// document.body.elItem.append(elLink)
+
+
+
+// //-------------------STYLE--------------//
+
+// elTitle.style.paddingTop = "61px"
